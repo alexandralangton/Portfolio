0ea091874cc7528f6bcc024e3f616a6952a51923
Alexandra Langton <alexandra.langton@outlook.com>
2020-12-19 22:40:13 -0500

    temp stash of cover photo version

--- app/components/Cover.js
+++ app/components/Cover.js
@@ -22,14 +22,29 @@ const Cover = (props) => {
 	}, []);
 
 	return (
-		<div id="about-container">
-			<img src={woods} id="cover-pic" alt="Alexandra walking in the woods" />
-			<h1 className="cover name">ALEXANDRA LANGTON</h1>
-			<h3 className="cover subheader">{subHeader}</h3>
-			<button type="button" onClick={() => props.setScroll(true)}>
-				Click me!
-			</button>
-		</div>
+		<>
+			<div id="about-container">
+				<img src={woods} id="cover-pic" alt="Alexandra walking in the woods" />
+				<h1 className="cover name">ALEXANDRA LANGTON</h1>
+				<h3 className="cover subheader">{subHeader}</h3>
+				<button
+					type="button"
+					className="scroll"
+					onClick={() => props.setScroll(true)}
+				>
+					LEARN MORE
+				</button>
+			</div>
+			<div id="temp">
+				<h1 style={{ marginTop: 0 }}>Nice to meet you!</h1>
+				<p style={{ textAlign: 'center' }}>
+					Lorem ipsum blah blah prentend I'm a paragraph, hi I'm Alex I like JS
+					and CSS and sometimes even some SQL. I used to like languages but I
+					got bored. ETC ETrkjglj kwrltk lkyt lk4yjlk lwky klwyk l4kyl4k 4lyk3l
+					k3yl.
+				</p>
+			</div>
+		</>
 	);
 };
 
