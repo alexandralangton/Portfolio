fea084ceae42a1003d5d1a5a6dca90f2cf5fdcc7
Alexandra Langton <alexandra.langton@outlook.com>
2020-12-26 13:52:45 -0500

    Replaced contact icons with font awesome icons

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -1,66 +1,38 @@
-import React, { useState } from 'react';
-import LinkedIn from '../images/LinkedIn.png';
-import LinkedInOrange from '../images/LinkedInOrange.png';
-import Email from '../images/Email.png';
-import EmailOrange from '../images/EmailOrange.png';
-import EmailOld from '../images/EmailOld.png';
-import EmailOrangeOld from '../images/EmailOrangeOld.png';
-import GitHub from '../images/GitHub.png';
-import GitHubOrange from '../images/GitHubOrange.png';
-import Medium from '../images/Medium.png';
-import MediumOrange from '../images/MediumOrange.png';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faMediumM,
+	faGithub,
+	faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
-	const [ghSrc, setGhSrc] = useState(GitHub);
-	const [liSrc, setLiSrc] = useState(LinkedIn);
-	const [emSrc, setEmSrc] = useState(Email);
-	const [mediumSrc, setMedSrc] = useState(Medium);
-
 	return (
 		<div id="contact-links">
 			<a href="mailto:alexandra.langton@outlook.com?subject=Hello!">
-				<img
-					src={emSrc}
-					onMouseEnter={() => setEmSrc(EmailOrange)}
-					onMouseLeave={() => setEmSrc(Email)}
-					className="logo"
-				/>
+				<FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
 			</a>
 			<a
 				href="https://www.linkedin.com/in/alexandralangton/"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<img
-					src={liSrc}
-					onMouseEnter={() => setLiSrc(LinkedInOrange)}
-					onMouseLeave={() => setLiSrc(LinkedIn)}
-					className="logo"
-				/>
+				<FontAwesomeIcon icon={faLinkedin} className="contact-icon" />
 			</a>
 			<a
 				href="https://github.com/alexandralangton"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<img
-					src={ghSrc}
-					onMouseEnter={() => setGhSrc(GitHubOrange)}
-					onMouseLeave={() => setGhSrc(GitHub)}
-					className="logo"
-				/>
+				<FontAwesomeIcon icon={faGithub} className="contact-icon" />
 			</a>
 			<a
 				href="https://alexandra-langton.medium.com/"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<img
-					src={mediumSrc}
-					onMouseEnter={() => setMedSrc(MediumOrange)}
-					onMouseLeave={() => setMedSrc(Medium)}
-					className="logo"
-				/>
+				<FontAwesomeIcon icon={faMediumM} className="contact-icon" />
 			</a>
 		</div>
 	);
